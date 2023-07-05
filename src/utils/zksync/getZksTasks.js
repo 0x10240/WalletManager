194981d6a16ca927fdb631abc5f290ec1efd7d8a
luoye <luoyeETH@gmail.com>
2023-07-05 09:17:50 +0800

    任务列表查询优化

--- src/utils/zksync/getZksTasks.js
+++ src/utils/zksync/getZksTasks.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 async function getZksTasks(address) {
     try {
-        let url = `https://block-explorer-api.mainnet.zksync.io/transactions?address=${address}&pageSize=10&page=1`;
+        let url = `https://block-explorer-api.mainnet.zksync.io/transactions?address=${address}&limit=100&page=1`;
         const response = await axios.get(url);
         const pageValue = parseInt(response.data.meta.totalPages);
         let contractAddresses = response.data.items.map(item => item["to"]);
         if (pageValue > 1) {
             contractAddresses = [];
             for (let i = 1; i <= pageValue; i++) {
-                const url = `https://block-explorer-api.mainnet.zksync.io/transactions?address=${address}&pageSize=10&page=${i}`;
+                const url = `https://block-explorer-api.mainnet.zksync.io/transactions?address=${address}&limit=100&page=${i}`;
                 const response = await axios.get(url);
                 let newcontractAddresses = response.data.items.map(item => item["to"]);
                 contractAddresses = contractAddresses.concat(newcontractAddresses);
