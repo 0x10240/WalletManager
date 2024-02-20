48466026aa32a5d2dfd5c9a65fbe57db4cae3f57
luoye <luoyeETH@gmail.com>
2024-02-20 10:59:20 +0800

    bugfix 修复交易金额统计失效

--- src/utils/linea/getLineaTx.js
+++ src/utils/linea/getLineaTx.js
@@ -37,16 +37,15 @@ function getZkSyncLastTX(lastTxDatetime) {
 
 const getEthPrice = async () => {
     try {
-        const response = await axios.post('https://mainnet.era.zksync.io/', {
-            id: 42,
-            jsonrpc: '2.0',
-            method: 'zks_getTokenPrice',
-            params: ['0x0000000000000000000000000000000000000000'],
-        });
-        return response.data.result
+        const options = {
+            method: 'GET',
+            url: 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD',
+        }
+        let response = await axios.request(options)
+        return response.data['USD']
     } catch (e) {
         console.log(e)
-        return 1870
+        return "/"
     }
 
 }
